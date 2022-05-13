725d3c544ff6f1fc29977a35c38d3d4040906097
Nikk <nikkigazejr@gmail.com>
2022-05-13 08:34:30 +0300

    Added character

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    [SerializeField] private Transform _playerTransform;
+
+    private Vector3 _playerOffset;
+    private void Start()
+    {
+        _playerOffset = transform.position - _playerTransform.position;
+    }
+
+    private void Update()
+    {
+        if (_playerTransform)
+        {
+            transform.position = _playerTransform.position + _playerOffset;
+        }
+    }
+}
