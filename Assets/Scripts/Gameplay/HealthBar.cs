b9421b7b4b98da208f8cfbfa18ddd913d4373de2
Nikk <nikkigazejr@gmail.com>
2022-05-18 22:39:15 +0300

    enemy healthbar

--- Assets/Scripts/Gameplay/HealthBar.cs
+++ Assets/Scripts/Gameplay/HealthBar.cs
@@ -3,23 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.UIElements;
+using Image = UnityEngine.UI.Image;
 
 public class HealthBar : MonoBehaviour
 {
     [SerializeField] private GameObject owner;
-    [SerializeField] private Text healthText;
     [SerializeField] private HealthStats healthSource;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private Image bar;
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = Camera.main.WorldToScreenPoint(owner.transform.position);
-        healthText.text = ((int)Mathf.Floor(healthSource.GetHP())).ToString();
+        transform.position = Camera.main.WorldToScreenPoint(owner.transform.position) + new Vector3(0, 80, 0);
+        bar.fillAmount = healthSource.GetHPPercent();
     }
 
 }
